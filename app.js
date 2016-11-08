da0eac0746f3086d2b5848bd3bacbc6605a09c59
Urvashi Gupta <codewithurvashi@gmail.com>
2016-11-07 22:43:13 -0800

    modified files

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.get("/reservation", function(req, res) {
    res.render("reservation"); 
 });
 
+app.get("/news", function(req, res) {
+   res.render("news"); 
+});
+
+app.get("/location", function(req, res) {
+   res.render("location"); 
+});
+
+app.get("/signup", function(req, res) {
+   res.render("signup"); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp server has started!");
